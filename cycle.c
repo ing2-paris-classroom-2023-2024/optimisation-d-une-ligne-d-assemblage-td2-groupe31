d95ff7d88caea5b02a94caed70461dd4c5b9cd95
evansiritham <evansiritham@gmail.com>
2023-11-29 14:56:20 +0100

    Contrainte de cycle

---
+++ cycle.c
@@ -0,0 +1,50 @@
+#include "header.h"
+#include "graphe.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void cycle (Graphe *graphe) {
+    int tab[graphe->ordre], verif = 0, i = 0, station = 0;
+    float somme = 0;
+    for (int i = 0; i < graphe->ordre; i++) {
+        tab[i] = i;
+    }
+    for (int i = 0; i < graphe->ordre - 1; i++) {
+        for (int j = 0; j < graphe->ordre - i - 1; j++) {
+            if (graphe->pSommet[tab[j]]->duree < graphe->pSommet[tab[j + 1]]->duree) {
+                int temp = tab[j];
+                tab[j] = tab[j + 1];
+                tab[j + 1] = temp;
+            }
+        }
+    }
+    do {
+        do {
+
+            if(tab[i] !=graphe->ordre + 1 ) {
+                if (graphe->pSommet[tab[i]]->duree + somme <= 10) {
+                    somme += graphe->pSommet[tab[i]]->duree;
+                    tab[i] = graphe->ordre + 1;
+                }
+            }
+            i++;
+        } while (i < graphe->ordre);
+        printf("somme : %f\n", somme);
+        i=0;
+        somme = 0;
+        station++;
+        verif = 1;
+        for (int i = 0; i < graphe->ordre; i++) {
+            if (tab[i] != graphe->ordre + 1) {
+                verif = 0;
+            }
+        }
+    } while (verif == 0);
+
+    printf("nb station : %d ", station);
+}
+
+
+
+
+
